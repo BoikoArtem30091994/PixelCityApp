723cfbe18afdd1630eab04c6f842acede58d6c28
Artem <boykoartem94@gmail.com>
2017-11-06 16:03:00 +0200

    Add tap gesture recognizer to drop pin on map. Also change default color for pin.

--- PixelCityApp/Controllers/MapVC.swift
+++ PixelCityApp/Controllers/MapVC.swift
@@ -24,8 +24,15 @@ class MapVC: UIViewController {
         mapView.delegate = self
         locationManager.delegate = self
         configureLocationServices()
+        addDoubletap()
     }
-
+    func addDoubletap(){
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector (dropPin(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+//        doubleTap.delegate = self
+        mapView.addGestureRecognizer(doubleTap)
+    }
+    
     //MARK: Actions
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
@@ -35,10 +42,38 @@ class MapVC: UIViewController {
 }
 
 extension MapVC: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
+        pinAnnotation.pinTintColor = #colorLiteral(red: 0.9769095778, green: 0.7193570733, blue: 0.1747315228, alpha: 1)
+        pinAnnotation.animatesDrop = true
+        return pinAnnotation
+    }
+    
     func centerMapOnUserLocation(){
         guard let coordinate = locationManager.location?.coordinate else {return}
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    @objc func dropPin(sender: UITapGestureRecognizer){
+        //drop the pin on map
+        removePin()
+        let touchPoint = sender.location(in: mapView)
+        let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
+        let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        mapView.addAnnotation(annotation)
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func removePin(){
+        for annotation in mapView.annotations {
+            mapView.removeAnnotation(annotation)
+        }
     }
 }
 
---
+++ PixelCityApp/View/DroppablePin.swift
@@ -0,0 +1,21 @@
+//
+//  DroppablePin.swift
+//  PixelCityApp
+//
+//  Created by Boiko Artem on 06.11.17.
+//  Copyright © 2017 Boiko Artem. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+    }
+    
+}
